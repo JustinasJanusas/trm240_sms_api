3757cc3f2d4a285fc8570639a9ae296ac3d6dd7b
studentas <justinas.janusas8@gmail.com>
2022-12-19 16:28:19 +0200

    added client

---
+++ client/client.py
@@ -0,0 +1,18 @@
+import zmq
+import sys
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(1):
+    print("Sending request %s …" % request)
+    socket.send(sys.argv[1].encode('utf-8'))
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message.decode('utf8')))
